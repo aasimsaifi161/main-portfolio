02b8a6845f01308ba9a1b342207ec86684cf8f92
Aasim <aasimsaifi161@gmail.com>
2026-01-04 22:01:16 +0530

    fixed imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import DateTime from './components/DateTime'
 import Social from './components/Social'
